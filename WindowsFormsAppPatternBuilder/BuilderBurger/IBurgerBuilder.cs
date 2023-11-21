672808c2a4ef41514fa26e946e0a95bff7d1da60
tester1 <tester1@test.ru>
2023-11-21 10:16:18 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsAppPatternBuilder/BuilderBurger/IBurgerBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsAppPatternBuilder.DBCon;
+
+namespace WindowsFormsAppPatternBuilder.BuilderBurger
+{
+    public interface IBurgerBuilder
+    {
+        IBurgerBuilder AddCheese();
+        IBurgerBuilder AddBacon();
+        IBurgerBuilder AddLettuce();
+        IBurgerBuilder AddTomato();
+        IBurgerBuilder AddPickles();
+        Burgers GetBurgers();
+    }
+}
